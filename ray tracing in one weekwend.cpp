ee0bd304252b751dd7ae6c7e3f91e29fbe26f4ec
spike-srj <84272678+spike-srj@users.noreply.github.com>
2022-05-14 17:00:38 +0800

    Update ray tracing in one weekwend.cpp

--- ray tracing in one weekwend.cpp
+++ ray tracing in one weekwend.cpp
@@ -19,6 +19,65 @@ int main() {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+    //显示进度
+    std::cerr << "\nDone.\n";
 }
 
+//对于linux或mac用户，先run一边上面的代码，会生成一个同名文件main，然后在当前目录的终端输入./main > image.ppm，就可生成一张图片
+./main > image.ppm
 
+
+创建vec3.h头文件，该文件在mac和win上运行会报错，只有虚拟机上可以不知道为啥
+
+#include <iostream>
+#include <cmath>
+class vec3 {
+    public:
+        //构造函数，第一行是无参构造函数，第二行是有参构造函数
+        vec3() : e{0,0,0} {}
+        vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+
+        double x() const { return e[0]; }
+        double y() const { return e[1]; }
+        double z() const { return e[2]; }
+
+        vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+        double operator[](int i) const { return e[i]; }
+        double& operator[](int i) { return e[i]; }
+
+        vec3& operator+=(const vec3 &v) {
+            e[0] += v.e[0];
+            e[1] += v.e[1];
+            e[2] += v.e[2];
+            return *this;
+        }
+
+        vec3& operator*=(const double t) {
+            e[0] *= t;
+            e[1] *= t;
+            e[2] *= t;
+            return *this;
+        }
+
+        vec3& operator/=(const double t) {
+            return *this *= 1/t;
+        }
+
+        double length() const {
+            return sqrt(length_squared());
+        }
+
+        double length_squared() const {
+            return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+        }
+
+        void write_color(std::ostream &out) {
+            // Write the translated [0,255] value of each color component.
+            out << static_cast<int>(255.999 * e[0]) << ' '
+                << static_cast<int>(255.999 * e[1]) << ' '
+                << static_cast<int>(255.999 * e[2]) << '\n';
+        }
+
+    public:
+        double e[3];
+};
