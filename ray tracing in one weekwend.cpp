1d88b7fcac8cd1b666e90fc41edeef26bbfa32b7
spike-srj <84272678+spike-srj@users.noreply.github.com>
2022-05-14 17:26:15 +0800

    Update ray tracing in one weekwend.cpp

--- ray tracing in one weekwend.cpp
+++ ray tracing in one weekwend.cpp
@@ -26,51 +26,52 @@ int main() {
 //对于linux或mac用户，先run一边上面的代码，会生成一个同名文件main，然后在当前目录的终端输入./main > image.ppm，就可生成一张图片
 ./main > image.ppm
 
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 创建vec3.h头文件，该文件在mac和win上运行会报错，只有虚拟机上可以不知道为啥
 
 #include <iostream>
 #include <cmath>
 class vec3 {
     public:
-        //构造函数，第一行是无参构造函数，第二行是有参构造函数
+        //构造函数，第一行是无参构造函数，第二行是有参构造函数，将e0，e1，e2传入了e{}中
         vec3() : e{0,0,0} {}
         vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
-
+        //类的函数
         double x() const { return e[0]; }
         double y() const { return e[1]; }
         double z() const { return e[2]; }
-
+        //操作符重载，无参操作
         vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+        //没看懂
         double operator[](int i) const { return e[i]; }
         double& operator[](int i) { return e[i]; }
-
+        //操作符重载，有参操作
         vec3& operator+=(const vec3 &v) {
             e[0] += v.e[0];
             e[1] += v.e[1];
             e[2] += v.e[2];
             return *this;
         }
-
+        //操作符重载，有参操作
         vec3& operator*=(const double t) {
             e[0] *= t;
             e[1] *= t;
             e[2] *= t;
             return *this;
         }
-
+        //操作符重载，有参操作
         vec3& operator/=(const double t) {
             return *this *= 1/t;
         }
-
+        //函数
         double length() const {
             return sqrt(length_squared());
         }
-
+        //函数
         double length_squared() const {
             return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
         }
-
+        //没看懂
         void write_color(std::ostream &out) {
             // Write the translated [0,255] value of each color component.
             out << static_cast<int>(255.999 * e[0]) << ' '
@@ -79,5 +80,140 @@ class vec3 {
         }
 
     public:
-        double e[3];
+        double e[3];  //属性
 };
+//第二部分
+// inline 是什么，这里也是操作符重载麻
+inline std::ostream& operator<<(std::ostream &out, const vec3 &v) {
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+inline vec3 operator+(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+inline vec3 operator-(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+inline vec3 operator*(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+inline vec3 operator*(double t, const vec3 &v) {
+    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
+}
+
+inline vec3 operator*(const vec3 &v, double t) {
+    return t * v;
+}
+
+inline vec3 operator/(vec3 v, double t) {
+    return (1/t) * v;
+}
+
+inline double dot(const vec3 &u, const vec3 &v) {
+    return u.e[0] * v.e[0]
+         + u.e[1] * v.e[1]
+         + u.e[2] * v.e[2];
+}
+
+inline vec3 cross(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+inline vec3 unit_vector(vec3 v) {
+    return v / v.length();
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//可以使用vec3类将我们的main函数改成这样
+#include "vec3.h"
+
+#include <iostream>
+
+int main() {
+    const int image_width = 200;
+    const int image_height = 100;
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = image_height-1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        for (int i = 0; i < image_width; ++i) {
+            vec3 color(double(i)/image_width, double(j)/image_height, 0.2);
+            color.write_color(std::cout);
+        }
+    }
+
+    std::cerr << "\nDone.\n";
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//创建ray.h
+#ifndef RAY_H
+#define RAY_H
+
+#include "vec3.h"
+
+class ray {
+    public:
+        //无参构造函数
+        ray() {}
+        //有参构造函数，将origin传给属性orig
+        ray(const vec3& origin, const vec3& direction)
+            : orig(origin), dir(direction)
+        {}
+        //函数，用来读取两个属性
+        vec3 origin() const    { return orig; }
+        vec3 direction() const { return dir; }
+        //函数，需要传入参数t，来计算光线t时的位置
+        vec3 at(double t) const {
+            return orig + t*dir;
+        }
+
+    public:
+        vec3 orig;  //属性，表示光的起点
+        vec3 dir;   //属性，表示光的方向
+};
+#endif
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//更新main.cpp
+#include "ray.h"
+
+#include <iostream>
+
+//未知的全局函数
+vec3 ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+}
+
+int main() {
+    //设置图像的宽和高
+    const int image_width = 200;
+    const int image_height = 100;
+
+    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+    vec3 lower_left_corner(-2.0, -1.0, -1.0);
+    vec3 horizontal(4.0, 0.0, 0.0);
+    vec3 vertical(0.0, 2.0, 0.0);
+    //摄像机的位置，也就是光线的起点，从这发出光，首先射向左下角
+    vec3 origin(0.0, 0.0, 0.0);
+    for (int j = image_height-1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        for (int i = 0; i < image_width; ++i) {
+            auto u = double(i) / image_width;
+            auto v = double(j) / image_height;
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+            vec3 color = ray_color(r);
+            color.write_color(std::cout);
+        }
+    }
+
+    std::cerr << "\nDone.\n";
+}
