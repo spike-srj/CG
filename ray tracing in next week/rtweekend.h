2fb83d620d023892bb6409933a347a9dbbbf6306
spike-srj <84272678+spike-srj@users.noreply.github.com>
2022-05-27 21:46:58 +0800

    Add files via upload

---
+++ ray tracing in next week/rtweekend.h
@@ -0,0 +1,59 @@
+#ifndef RTWEEKEND_H
+#define RTWEEKEND_H
+
+#include <cmath>
+#include <cstdlib>
+#include <limits>
+#include <memory>
+
+
+// Usings
+
+using std::shared_ptr;
+using std::make_shared;
+
+// Constants
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// Utility Functions
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180;
+}
+
+inline double ffmin(double a, double b) { return a <= b ? a : b; }
+inline double ffmax(double a, double b) { return a >= b ? a : b; }
+
+inline double random_double() {
+    // Returns a random real in [0,1).
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline int random_int(int a, int b) {
+    // Returns a random real in [a,b].
+    return (rand() % (b-a+1))+ a;
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max-min)*random_double();
+}
+
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
+}
+
+
+
+
+
+// Common Headers
+
+#include "ray.h"
+#include "vec3.h"
+
+#endif
