efec89ed2d3f8a8bf6fb6333a3db11f7a3c2b8a6
spike-srj <84272678+spike-srj@users.noreply.github.com>
2022-06-02 13:02:23 +0800

    Add files via upload

---
+++ instance/hittable.h
@@ -0,0 +1,73 @@
+#ifndef HITTABLE_H
+#define HITTABLE_H
+
+#include "ray.h"
+
+class material;   //??????????????????
+
+class aabb {
+    public:
+        aabb() {}
+        aabb(const vec3& a, const vec3& b) { _min = a; _max = b;}
+
+        vec3 min() const {return _min; }
+        vec3 max() const {return _max; }
+        inline bool hit(const ray& r, double tmin, double tmax) const {
+            for (int a = 0; a < 3; a++) {
+                auto invD = 1.0f / r.direction()[a];
+                auto t0 = (min()[a] - r.origin()[a]) * invD;
+                auto t1 = (max()[a] - r.origin()[a]) * invD;
+                if (invD < 0.0f)
+                    std::swap(t0, t1);
+                tmin = t0 > tmin ? t0 : tmin;
+                tmax = t1 < tmax ? t1 : tmax;
+                if (tmax <= tmin)
+                    return false;
+            }
+            return true;
+        }
+        
+
+
+        vec3 _min;
+        vec3 _max;
+};
+
+aabb surrounding_box(aabb box0, aabb box1) {
+    vec3 small(ffmin(box0.min().x(), box1.min().x()),
+               ffmin(box0.min().y(), box1.min().y()),
+               ffmin(box0.min().z(), box1.min().z()));
+    vec3 big  (ffmax(box0.max().x(), box1.max().x()),
+               ffmax(box0.max().y(), box1.max().y()),
+               ffmax(box0.max().z(), box1.max().z()));
+    return aabb(small,big);
+}
+
+
+struct hit_record {
+    vec3 p;
+    vec3 normal;
+    double t;
+    bool front_face;
+    double u;
+    double v;
+    shared_ptr<material> mat_ptr;
+
+    inline void set_face_normal(const ray& r, const vec3& outward_normal) {
+        front_face = dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal :-outward_normal;
+    }
+};
+
+class hittable {
+    public:
+        virtual bool hit(
+            const ray& r, double t_min, double t_max, hit_record& rec) const = 0;
+        virtual bool bounding_box(double t0, double t1, aabb& output_box) const = 0;
+};
+
+
+
+
+
+#endif
